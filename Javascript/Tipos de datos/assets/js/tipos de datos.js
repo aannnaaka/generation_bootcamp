aae028de335d2e7611af08cae8c52bb9a324ccfd
AnaMatias <matias_6im6@yahoo.com.mx>
2022-04-26 12:46:04 -0500

    pagina completa

---
+++ Javascript/Tipos de datos/assets/js/tipos de datos.js
@@ -0,0 +1,14 @@
+let nombre = "Jonathan";//string
+let edad = 23;//number
+let dinero = 23.3;//number;
+
+let tienesTrabajo = true;//Boolean
+let variableNull = null;//nulo
+
+let variableIndefinida;
+
+console.log(typeof(nombre));
+console.log(typeof(dinero));
+console.log(typeof(tienesTrabajo));
+console.log(typeof(variableNull));
+console.log(typeof(variableIndefinida));
